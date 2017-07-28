6a1d95a00ed73bd8857f703e66e17f428c3f301c
Ryota Kobayashi <rytakov@gmail.com>
2017-07-28 12:29:19 +0900

    Add comments

--- let.fish
+++ let.fish
@@ -1,14 +1,14 @@
 function let --no-scope-shadowing
-    if not string match -qr '^\w+' -- "$argv"
+    if not string match -qr '^\w+' -- "$argv" # variable name is not detected
         bash -c "let $argv[1]"
         return $status
     end
-
-    if string match -qr '^argv\W' -- "$argv" # argv
+    
+    if string match -qr '^argv\W' -- "$argv" # for argv
         set -l expr (string match -r '(\w+)(.*)' -- "$argv")
         set -e argv
         set argv (bash -c "v=$$expr[2]; let v$expr[3]; echo \$v")
-    else # other variables
+    else # for other variables
         set argv (string match -r '(\w+)(.*)' -- "$argv")
         set $argv[2] (bash -c "v=$$argv[2]; let v$argv[3]; echo \$v")
     end
